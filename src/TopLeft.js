44e57ad31bf616f367bb92e0f8c0703e671a08bf
Hee Won Chung <heewonchung64@gmail.com>
2018-10-20 15:17:52 -0700

    TopLeft

---
+++ src/TopLeft.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {Button} from "react";
+
+export class TopLeft extends Component {
+    render() {
+        return (
+            <div>
+                <Button title="+" />
+            </div>
+        );
+    }
+}
