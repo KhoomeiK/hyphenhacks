9a23876340d7938e10b35a8a7a4e07e5ba375aae
AbhinavT2 <abhinav.tumu1@gmail.com>
2018-10-21 04:49:01 -0700

    all allign

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import TopRight from "./components/TopRight";
 import TopLeft from "./components/TopLeft";
-import { Container, Row, Col } from "reactstrap";
+//import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col } from 'react-grid-system';
 // import Columns from 'react-columns';
 // import ReactScrollableList from "react-scrollable-list";
 
@@ -12,10 +13,10 @@ class App extends Component {
       <div>
         <Container>
           <Row>
-            <Col>
+            <Col sm={4}>
               <TopLeft fb={this.props.fb} />
             </Col>
-            <Col>
+            <Col sm={8}>
               <TopRight fb={this.props.fb} />
             </Col>
           </Row>
--- src/components/TopRight.js
+++ src/components/TopRight.js
@@ -1,47 +1,9 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
-// import './TopRight.css';
+//import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col } from 'react-grid-system';
+import './TopRight.css';
 
 export default class TopRight extends Component {
-<<<<<<< HEAD
-    render() {
-        const divStyle = {
-            // position: 'relative',
-            // right: '20px',
-            // width: '750px',
-            // height: '60px',
-            // padding: '20px',
-            // margin: '0px',
-            // // float: 'right',
-            // fontSize: '200%',
-            // paddingLeft: '50px',
-        }
-        const buttonStyle = {
-            // fontSize: '100%',
-            // backgroundColor: '#00BFFF',
-            // fontFamily: './font.ttf',
-            // color: 'black',
-            // margin: '10px',
-            // padding: '10px'
-        }
-        return (
-            <Row>
-                <Col>
-                    <div style={divStyle}>
-                        <h1> SUMMA </h1>
-                        <span style={buttonStyle}>Summary </span>
-                        <span style={buttonStyle}>Transcript </span>
-                        <span style={buttonStyle}>Recording </span>
-                    </div>
-                </Col>
-                <Col>
-                    <button onClick={this.props.fb.signin}> signin </button>
-                </Col>
-            </Row>
-        );
-    }
-}       
-=======
   render() {
     const divStyle = {
       // position: 'relative',
@@ -53,20 +15,45 @@ export default class TopRight extends Component {
       // // float: 'right',
       // fontSize: '200%',
       // paddingLeft: '50px',
+      // <img stylesrc="i.imgur.com/dLElTb1.jpg" alt="Logo" />
     };
+    const buttonStyle = {
+        
+       
+        
+        marginRight: '10px',
+        backgroundColor: '#33CCFF',
+        borderRadius: '2px',
+        color: 'white',
+        fontSize: '24px',
+        fontFamily:'font1',
+    
+
+       
+    }
     return (
       // <Container>
-      <Row>
+      <Row style={{gridColumns:'2'}}>
         <Col>
+        <button style={{position:'relative',float:'right',}}> signin </button>
           <div style={divStyle}>
-            <img src="logo2.PNG" alt="Logo" />
-            <button className="buttonStyle">Summary </button>
-            <button className="buttonStyle">Transcript </button>
-            <button className="buttonStyle">Recording </button>
+            <h2 style={{position:'relative',left:'240px'}}>SUMMA</h2>
+            <div style={{gridColumns:'3'}}>
+            <Row>
+                <Col>
+                <button style={buttonStyle}>SUMMARY </button>
+                </Col>
+                <Col >
+                <button style={buttonStyle}>TRANSCRIPT </button>
+                </Col>
+                <Col>
+                <button style={buttonStyle}>RECORDING </button>
+                </Col>
+            </Row>
+            </div>
+            
+            
           </div>
-        </Col>
-        <Col>
-          <button> signin </button>
         </Col>
       </Row>
       // </Container>
@@ -74,4 +61,3 @@ export default class TopRight extends Component {
     // onClick={this.props.fb.signin}
   }
 }
->>>>>>> 3a37d10373235544886c6ee9f6891099a6b35e41
