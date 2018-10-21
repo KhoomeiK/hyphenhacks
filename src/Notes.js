e59256646b2d32e3468f37032c3d086ce29d91af
Hee Won Chung <heewonchung64@gmail.com>
2018-10-20 17:06:34 -0700

    Notes

--- src/Notes.js
+++ src/Notes.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 
 export default class Notes extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     loopRender() {
         let change =  [];
