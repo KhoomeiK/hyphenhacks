3bd6006303d8d00aa624d2e37c4da75f8b38fc47
Andrew Tedijanto <theglobalcaucus@gmail.com>
2018-10-20 18:17:24 -0700

    Fixed TopLeft.js errors

--- src/TopLeft.js
+++ src/TopLeft.js
@@ -2,42 +2,46 @@ import React, { Component } from "react";
 import Notes from "./Notes";
 
 export class TopLeft extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {array:[
-            {
-                id: "234",
-                title: "neckrope",
-                summary: "time to neckrope"
-            },
-            {
-                id: "2345",
-                title: "double neckrope",
-                summary: "sex"
-            },
-            {
-                id: "23456",
-                title: "complete sex",
-                summary: "time to completely sex"
-            }
-        ]};
-    }
+    this.state = {
+      array: [
+        {
+          id: "234",
+          title: "neckrope",
+          summary: "time to neckrope"
+        },
+        {
+          id: "2345",
+          title: "double neckrope",
+          summary: "sex"
+        },
+        {
+          id: "23456",
+          title: "complete sex",
+          summary: "time to completely sex"
+        }
+      ]
+    };
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <div className="navBar">
-                    <button className="Plus-button" onClick={}>+</button>
-                    <div className="search-container">
-                        <form>
-                            <input type="text" name="name" placeholder="Search..." />
-                        </form>
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                    </div>
-                </div>
-                <Notes array={this.state.array}/>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="navbar">
+          <button className="Plus-button">+</button>
+          <div className="search-container">
+            <form>
+              <input type="text" name="name" placeholder="Search..." />
+            </form>
+            <button type="submit">
+              <i class="fa fa-search" />
+            </button>
+          </div>
+        </div>
+        <Notes array={this.state.array} />
+      </React.Fragment>
+    );
+  }
 }
